3b402b923902b6344654fabbd5cec902baec6bd0
jahirubc <jahirubc@gmail.com>
2023-05-10 13:01:16 +0600

    user management servlet added

---
+++ user-management-servlet/src/main/java/com.pondit/com/pondit/HelloServlet.java
@@ -0,0 +1,24 @@
+package com.pondit;
+import java.io.IOException;
+
+import com.pondit.model.User;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "helloServlet", value = "/")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	String name = req.getParameter("name");
+    	Integer age = Integer.parseInt(req.getParameter("age"));
+    	//User user = new User("Syed", 30);
+    	User user = new User(name, age);
+    	req.setAttribute("user", user);  
+    	req.getRequestDispatcher("hello.jsp").forward(req, resp);
+    }
+}
---
+++ user-management-servlet/src/main/webapp/hello.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>Hello from the other side</h1>
+<p>Name: ${user.name} </p>
+<p>Age: ${user.age} </p>
+</body>
+</html>
