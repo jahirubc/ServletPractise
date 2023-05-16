ea4f8b44340c72b7ad41cd669a17e0089f732f31
jahirubc <jahirubc@gmail.com>
2023-05-17 00:23:56 +0600

    tried to solve builder error

--- user-management-servlet/src/main/java/com/pondit/model/User.java
+++ user-management-servlet/src/main/java/com/pondit/model/User.java
@@ -10,6 +10,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
@@ -29,4 +30,5 @@ public class User {
 	public User() {
 		
 	}
+	
 }
---
+++ user-management-servlet/src/main/java/configuration/hibernate/HibernateConfig.java
@@ -0,0 +1,6 @@
+package configuration.hibernate;
+
+
+public class HibernateConfig {
+
+}
